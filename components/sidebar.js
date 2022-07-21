7a06c9d81c5c9a06dc641203f2b85d10519765ed
phamill289 <hamillp1@go.stockton.edu>
2022-07-20 23:43:29 -0400

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -9,6 +9,7 @@ export function Sidebar() {
     return (
         // bg='#909296'
         // bg='orange.400'
+
         <Center w='30%' bg='#ED8936' alignItems="center" p={25} fontFamily='Inconsolata' fontStyle='normal' fontWeight='bold' >
 
             <Stack alignContent='center' textAlign='center' >
@@ -22,38 +23,39 @@ export function Sidebar() {
                         ease: [0, 0.71, 0.2, 1.01]
                     }}
                 >
+
                     <AspectRatio maxW='256' ratio={4 / 4}>
                         <Image src='/toger4.png' objectFit='contain' />
                     </AspectRatio>
+
                 </motion.div>
 
 
 
 
+                <Text  >Patrick Hamill</Text>
+                <Text >hamillp1@go.stockton.edu</Text>
+                <Text >portfolio website</Text>
+                <HStack >
+                    <Link href='https://github.com/phamill289'>
+                        <a>
+                            <Image
+                                boxSize={'100'}
+                                src='https://joshuapenalba.files.wordpress.com/2014/12/github-icon.png?w=640'
 
-                    <Text  >Patrick Hamill</Text>
-                    <Text >hamillp1@go.stockton.edu</Text>
-                    <Text >portfolio website</Text>
-                    <HStack >
-                        <Link href='https://github.com/phamill289'>
-                            <a>
-                                <Image
-                                    boxSize={'100'}
-                                    src='https://joshuapenalba.files.wordpress.com/2014/12/github-icon.png?w=640'
+                            />
+                        </a>
+                    </Link>
+                    <Link href='mailto:hamillp1@go.stockton.edu'>
+                        <a>
+                            <Image
+                                boxSize={'81'}
+                                src='/email2.png'
 
-                                />
-                            </a>
-                        </Link>
-                        <Link href='mailto:hamillp1@go.stockton.edu'>
-                            <a>
-                                <Image
-                                    boxSize={'81'}
-                                    src='/email2.png'
-
-                                />
-                            </a>
-                        </Link>
-                    </HStack>
+                            />
+                        </a>
+                    </Link>
+                </HStack>
             </Stack>
         </Center>
 
