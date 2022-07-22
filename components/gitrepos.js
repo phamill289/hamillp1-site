77bec65921ffec2c100fb09e49a8981ed38d5399
phamill289 <hamillp1@go.stockton.edu>
2022-07-22 00:22:31 -0400

    Update gitrepos.js
    handle github api json via map instead of hard coding

--- components/gitrepos.js
+++ components/gitrepos.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Navbar from '../components/navbar.js'
 import Gitprofile from '../components/gitprofile.js'
+import React from 'react';
 import { Image, Grid, GridItem, Text, Flex, HStack } from '@chakra-ui/react'
 import useSWR from 'swr'
 import { motion } from 'framer-motion'
@@ -14,18 +15,18 @@ export function Gitrepos(props) {
 
 
     const { data: data, error: error } = useSWR('https://api.github.com/users/phamill289/repos', fetcher)
-    const { data: sjrail, error: sjrailerror } = useSWR('https://api.github.com/repos/phamill289/sjrail-openlayers', fetcher)
+    // const { data: sjrail, error: sjrailerror } = useSWR('https://api.github.com/repos/phamill289/sjrail-openlayers', fetcher)
     const { data: user, error: usererror } = useSWR('https://api.github.com/users/phamill289', fetcher)
-    const { data: site, error: siteerror } = useSWR('https://api.github.com/repos/phamill289/hamillp1-site', fetcher)
+
     if (error) return <div> &apos; data. An error has occurred.&apos; </div>;
     if (!data) return <div>  &apos; Loading...&apos;</div>;
 
 
-   
-    if (sjrailerror) return <div> &apos;data. An error has occurred.&apos; </div>;
-    if (!sjrail) return <div>  &apos; Loading...&apos; </div>;
 
-   
+    // if (sjrailerror) return <div> &apos;data. An error has occurred.&apos; </div>;
+    // if (!sjrail) return <div>  &apos; Loading...&apos; </div>;
+
+
     if (usererror) return <div> &apos; data. An error has occurred.&apos;</div>;
     if (!user) return <div>  &apos;Loading...&apos; </div>;
     const datalength = Object.keys(data).length
@@ -35,122 +36,124 @@ export function Gitrepos(props) {
 
         <motion.div
 
-        initial={{ opacity: 0, scale: 0.9 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-            duration: 0.5,
-            delay: 0,
-            ease: [0, 0.71, 0.2, 1.01]
-        }}
-    >
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.5,
+                delay: 0,
+                ease: [0, 0.71, 0.2, 1.01]
+            }}
+        >
+            {/* <div>
+      {data &&
+        data.map((item, i) => (
+          <div key={i}>
+            {data[i].html_url}
+          </div>
+        ))
+      }
+    </div> */}
+
+            <Grid templateColumns='repeat(5, 1fr)' gap={4} >
+                <GridItem colSpan={5} h='150' bg='#373A40' paddingLeft={'15px'} paddingTop={'7.5'} rounded='md'>
+                    <HStack >
+                        <Image className='rudy'
 
 
-        <Grid templateColumns='repeat(5, 1fr)' gap={4} >
-            <GridItem colSpan={5} h='150' bg='#373A40' paddingLeft={'15px'} paddingTop={'7.5'} rounded='md'>
-                <HStack >
-                    <Image className='rudy'
-
-
-                        boxSize={'135px'}
-                        src={data[0].owner.avatar_url}
-                        objectFit='cover'
-                    />
-                    <Link href='https://github.com/phamill289'>
-                        <a>
-                            <Text fontSize={'32px'} fontWeight='normal' textColor={'white'}> phamill289 <Text fontSize={'16px'}>following: {user.following} </Text> <Text fontSize={'16px'}>followers: {user.followers}</Text>  </Text >
-                        </a>
-                    </Link>
-
-
-
-                </HStack>
-
-            </GridItem>
-            <GridItem colStart={1} colEnd={6} h='600' bg='#373A40' rounded='md'>
-
-                <Grid templateColumns={'repeat(3, 1fr)'} gap={15} padding='8px'>
-                    <Link href={data[2].html_url}>
-                        <a>
-                            <GridItem h='130' bg='#ED8936' border='1px' rounded={'xl'} >
-
-                                <Text textColor={'white'} fontSize='20px' paddingLeft={'5px'}>
-
-                                    {data[2].name}
-
-                                </Text>
-
-                                <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
-                                    subscribers: {sjrail.subscribers_count}
-
-                                </Text>
-                                <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
-                                    watchers: {sjrail.watchers_count}
-                                </Text>
-                            </GridItem>
-
-                        </a>
-                    </Link>
-
-                    <Link href={data[0].html_url}>
-                        <a>
-                            <GridItem h='130' bg='#ED8936' border='1px' rounded={'xl'} >
-
-                                <Text textColor={'white'} fontSize='20px' paddingLeft={'5px'}>
-
-                                    {data[0].name}
-
-                                </Text>
-
-                                <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
-                                    subscribers: {site.subscribers_count}
-
-                                </Text>
-                                <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
-                                    watchers: {site.watchers_count}
-                                </Text>
-                            </GridItem>
-
-                        </a>
-                    </Link>
-
-                    <GridItem h='130' bg='#ED8936' border='1px' rounded={'xl'} >
-
-                        <Text textColor={'white'} fontSize='20px' paddingLeft={'5px'}>
-
-                            pbot
-
-                        </Text>
-
-                        <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
-                            private repo
-
-                        </Text>
-                         
-                    </GridItem>
-
-                    <GridItem h='130' bg='#ED8936' border='1px' rounded={'xl'} >
-
-                        <Text textColor={'white'} fontSize='20px' paddingLeft={'5px'}>
-
-                            ImageSorter-
-
-                        </Text>
-
-                        <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
-                            private repo
-
-                        </Text>
-                         
-                    </GridItem>
+                            boxSize={'135px'}
+                            src={data[0].owner.avatar_url}
+                            objectFit='cover'
+                        />
+                        <Link href='https://github.com/phamill289'>
+                            <a>
+                                <Text fontSize={'32px'} fontWeight='normal' textColor={'white'}> phamill289 <Text fontSize={'16px'}>following: {user.following} </Text> <Text fontSize={'16px'}>followers: {user.followers}</Text>  </Text >
+                            </a>
+                        </Link>
 
 
 
-                </Grid>
+                    </HStack>
 
-            </GridItem>
+                </GridItem>
 
-        </Grid>
-</motion.div>
+
+                <GridItem colStart={1} colEnd={6} h='600' bg='#373A40' rounded='md'>
+
+                    <Grid templateColumns={'repeat(3, 1fr)'} gap={15} padding='8px'>
+
+
+                        {data &&
+                            data.map((item, i) => (
+
+
+                                <Link href={data[i].html_url}>
+                                    <a>
+                                        <GridItem h='130' bg='#ED8936' border='1px' rounded={'xl'} >
+                                            <div key={i}>
+                                                <Text textColor={'white'} fontSize='20px' paddingLeft={'5px'}>
+
+                                                    {data[i].name}
+
+                                                </Text>
+                                                <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
+                                                    description: {data[i].description}
+
+                                                </Text>
+                                                <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
+                                                    language: {data[i].language}
+                                                </Text>
+                                                <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
+                                                    updated on: {data[i].updated_at}
+                                                </Text>
+                                            </div>
+                                        </GridItem>
+                                    </a>
+                                </Link>
+                            ))
+
+                        }
+
+
+
+
+                        <GridItem h='130' bg='#ED8936' border='1px' rounded={'xl'} >
+
+                            <Text textColor={'white'} fontSize='20px' paddingLeft={'5px'}>
+
+                                pbot
+
+                            </Text>
+
+                            <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
+                                private repo
+
+                            </Text>
+
+                        </GridItem>
+
+                        <GridItem h='130' bg='#ED8936' border='1px' rounded={'xl'} >
+
+                            <Text textColor={'white'} fontSize='20px' paddingLeft={'5px'}>
+
+                                ImageSorter-
+
+                            </Text>
+
+                            <Text textColor={'white'} paddingLeft={'5px'} fontWeight='normal'>
+                                private repo
+
+                            </Text>
+
+                        </GridItem>
+
+
+
+                    </Grid>
+
+                </GridItem>
+
+            </Grid>
+        </motion.div>
     )
 
 }
